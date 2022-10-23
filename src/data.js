25116996a58e6a3d98372cebdef848a610400a84
Williams Ajayi Balogun <williamsdev.official@gmail.com>
2022-10-23 23:14:32 +0100

    Edited App.js file and index.css file, created data.js file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        name: 'Twitter Link',
+        url: 'https://twitter.com/willhemzDev?t=8lt4cARSXgkHPjbVimA-mQ&s=09',
+        id: 'twitter-profile'
+    },
+    {
+        name: 'Zuri Team',
+        url: 'https://training.zuri.team/',
+        id: 'btn_zuri'
+    },
+    {
+        name: 'Zuri Books',
+        url: 'http://books.zuri.team/',
+        id: 'books'
+    }
+    {
+        name: 'Python Books',
+        url: 'https://books.zuri.team/python-for-beginners?ref_id=<Willhemz>',
+        id: 'book_python'
+    },
+    {
+        name: 'Background Check for Coders',
+        url: 'https://background.zuri.team/',
+        id: 'pitch'
+    },
+    {
+        name: 'Design Books',
+        url: 'https://books.zuri.team/design-rules',
+        id: 'book_design'
+    }
+]
