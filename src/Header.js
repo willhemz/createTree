232ae877866853b1133cabbb6ff9b65d4bc75c91
Williams Ajayi Balogun <williamsdev.official@gmail.com>
2022-10-28 15:48:08 +0100

    made corrections

--- src/Header.js
+++ src/Header.js
@@ -5,13 +5,13 @@ import {RiShareForwardLine} from 'react-icons/ri'
 const Header = () => {
     const [slack, setSlack] = useState(false)
   return <>
-    <header className="row px-5 py-2 pt-5" style={{background:'rgba(255, 230, 230, 0)'}}>
-        <nav className="header-data col-12 d-flex flex-column justify-content-center align-items-center position-relative" style={{perspective:'20rem'}}>
-            <img id='profile__img' className='rounded-circle img-fluid shadow-lg mb-3' src="./image/one.jpg" alt="Willhemz" style={{width:'10rem', height:'10rem', objectFit:'cover'}} />
-            <p id='twitter' className='fs-3 text-dark fw-normal mb-0 pb-0'>willhemzDev</p>
-            <p id='slack' type='button' className='fs-1 text-dark invisible' onClick={() => setSlack(!slack)}>{slack ? '*******' : 'WilliamsBalogun'}</p>
+    <header className="row px-5 pt-5">
+        <nav className="header-data col-12 d-flex flex-column justify-content-center align-items-center position-relative">
+            <img id='profile__img' className='rounded-circle img-fluid shadow-lg mb-3' src="./image/one.jpg" alt="Willhemz" style={{width:'8rem', height:'8rem', objectFit:'cover'}} />
+            <p id='twitter' className='fs-3 text-dark fw-bold mb-0 pb-0'>willhemzDev</p>
+            <p id='slack' type='button' className='fs-3 text-dark d-none' onClick={() => setSlack(!slack)}>{slack ? '*******' : 'WilliamsBalogun'}</p>
             <SlOptions className='fs-1 d-sm-none p-1 rounded-circle text-muted position-absolute top-0 start-100 translate-middle' style={{marginTop:'2rem',marginLeft:'-8%', border:'0.1rem dotted rgba(0,0,0,0.2)'}}/>
-            <RiShareForwardLine className='share-bar fs-1 d-none d-sm-block position-absolute top-0 start-100 translate-middle border p-2 rounded-5' style={{marginTop:'2rem',marginLeft:'-20%', transformStyle:'preserve-3d', transform:'rotateZ(180deg)'}}/>
+            <RiShareForwardLine className='share-bar fs-1 d-none d-sm-block position-absolute top-0 start-100 translate-middle border p-2 rounded-5' style={{marginTop:'2rem',marginLeft:'-20%'}}/>
         </nav>
     </header>
   </>
--- src/Main.js
+++ src/Main.js
@@ -4,19 +4,18 @@ import {AiOutlineGithub} from 'react-icons/ai'
 
 const Main = () => {
   return <>
-    <main className='row my-5 mt-0 mx-0'>
+    <main className='row mx-0 my-3'>
         <section className="col-10 my-0 mx-auto list-group">
             {
                 data.map((item, index) => {
                     const {name, url, id, title} = item
                     return <>
-                        <a className='border-0 text-center fw-bold fs-6 rounded-3 p-3 list-group-item list-group-item-secondary list-group-item-action my-2' id={id} key={index} title={title} href={url}>{name}</a>    
+                        <a className='border-0 text-center fw-normal fs-6 rounded-3 p-3 list-group-item list-group-item-secondary list-group-item-action my-2' id={id} key={index} title={title} href={url}>{name}</a>    
                     </>
                 })
             }
         </section>
         <section className="col-10 my-3 fs-4 mx-auto text-center">
-            {/* <SiSlack className='me-3 text-success'/> */}
             <img src="./image/slack.svg" alt="slack" className='col-1 me-3' style={{width:'1.6rem'}} />
             <a href="https://github.com/willhemz/webtree">
                 <AiOutlineGithub className='text-dark' style={{fontSize:'2rem'}} />
