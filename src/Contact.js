ac0172151c9a4f8eb0680bd8079185119c202c5e
Williams Ajayi Balogun <williamsdev.official@gmail.com>
2022-11-04 13:09:41 +0100

    style: added index.css to control the scaling of the contact form in small and large screen sizes by using CSS @media screen and (max-width:576px).

--- src/Contact.js
+++ src/Contact.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const Contact = () => {
-  return <main className='row mx-0 my-3 d-flex justify-content-center align-content-center' style={{height:'70vh'}}>
-    <section className='col-6'>
+  return <main className='row mx-0 my-3 d-flex justify-content-center align-content-center w-100' >
+    <section className='contact-page col-12 col-sm-9'>
         <div className="form-header">
             <h2>Contact Me</h2>
             <p>Hi there, contact me to ask me about anything you have in mind.</p>
         </div>
         <form className='mt-5 col-12'>
-            <div className="row mb-3 name-section">
-                <div className='col-6'>
+            <div className="row name-section">
+                <div className='col-12 col-sm-6 mb-3'>
                     <label className='form-label' htmlFor="first_name">First name</label>
                     <input id='first_name' type="text" placeholder='Enter your first name' className='form-control' />
                 </div>
-                <div className='col-6'>
+                <div className='col-12 col-sm-6 mb-3'>
                     <label className='form-label' htmlFor="last_name">Last name</label>
                     <input id='last_name' type="text" placeholder='Enter your last name' className='form-control' />
                 </div>
@@ -24,14 +24,14 @@ const Contact = () => {
             </div>
             <div className="message-section mb-3">
                 <label htmlFor="message" className='form-label'>Message</label>
-                <textarea name="message" id="message" className='form-control' rows="3"></textarea>
+                <textarea name="message" id="message" className='form-control' rows="3" style={{resize:'none'}}></textarea>
             </div>
-            <div className="form-check">
+            <div className="form-check mb-5">
                 <input className='form-check-input' type="checkbox" id="checkbox" />
                 <label htmlFor="checkbox" className='form-check-label'>You agree to providing your data to &#123;name&#125; who may contact you.</label>
             </div>
             <div className="submit-btn">
-                
+                <button className='btn btn-primary w-100' type="submit" id='btn__submit'>Send Message</button>
             </div>
         </form>
     </section>
